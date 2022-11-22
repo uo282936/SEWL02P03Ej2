cb9d4613a6aab94c597dfaf0591f6d2e697f770f
uo282936 <113896506+uo282936@users.noreply.github.com>
2022-11-22 17:49:58 +0100

    Add files via upload

---
+++ MasInfoNavegador.js
@@ -0,0 +1,5 @@
+document.write(infoNavegador.version + "</p>");
+document.write("<p>" + infoNavegador.plataforma + "</p>");
+document.write("<p>" + infoNavegador.vendedor + "</p>");
+document.write("<p>" + infoNavegador.agente + "</p>");
+document.write("<p>" + infoNavegador.javaActivo);
